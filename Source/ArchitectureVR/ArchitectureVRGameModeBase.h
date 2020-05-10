f70704547b95082d65672bd6de8ad19a9a9dfe08
Saeed Safikhani <s.safikhani@outlook.com>
2020-05-11 01:02:32 +0200

    Initial Commit

---
+++ Source/ArchitectureVR/ArchitectureVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ArchitectureVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHITECTUREVR_API AArchitectureVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
